fc2d0fa1538cd5222045257bc0af6dab28a9403d
Florencia Tarditti <maria.florencia.tarditti@est.fib.upc.edu>
2015-10-20 13:55:12 +0200

    Filter lines and start ordering them

--- routes/stations.js
+++ routes/stations.js
@@ -2,18 +2,24 @@ var stationsRouter = require('express').Router();
 var request = require('request');
 var _ = require('lodash');
 
+function compareLines(first, second) {
+  if (first.length <= second.length) return -1;
+  else if (first <= second) return -1;
+  return 1;
+}
+
 stationsRouter.get('/', function(req, res, next) {
     request('http://barcelonaapi.marcpous.com/metro/stations.json', function (error, response, body) {
         if (!error && response.statusCode == 200) {
-            console.log(body);
-            var data = JSON.parse(body.data);
-            console.log("sdaf");
-            console.log(data);
+            var body = JSON.parse(body);
+            var data = body.data;
             var metroData = data.metro;
             var lines = [];
             for (var i = 0; i < metroData.length; ++i) {
-                lines.push(metroData[i].line);
+              lines.push(metroData[i].line);
             }
+            lines = _.uniq(lines);
+            lines.sort(compareLines);
             res.status(200).json(lines);
         } else {
             console.log(error);
